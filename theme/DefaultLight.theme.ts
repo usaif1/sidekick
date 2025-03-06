a2d51971b51e080f04106efe0dd9d6cee6cb3a5d
Sahil Ahmed <sa.idesofmarch@gmail.com>
2025-03-06 21:46:41 +0530

    colors fixes

--- theme/DefaultLight.theme.ts
+++ theme/DefaultLight.theme.ts
@@ -1,29 +1,43 @@
 import { ColorTheme, Theme } from './Theme.type';
 
+/**
+ * Default light theme color palette
+ * Contains all the color values used throughout the application in light mode
+ */
 const DEFAULT_LIGHT_COLOR_THEME: ColorTheme = {
-    background: 'hsl(0, 0%, 100%)',
-    backgroundSecondary: 'hsl(0, 0%, 95%)', // Light grey background
-    primary: '#f78620',
-    primaryGrey: 'hsl(0, 0%, 37%)',
-    secondaryDark: 'hsl(0, 0%, 0%)',
-    borderGrey: '#DBDBDB',
-    textBlack: 'hsl(0, 0%, 7.06%)',
-    secondary: ' hsl(48, 91%, 61%)',
-    red: '#F11111',
-    green: ' hsl(144, 68%, 48%)',
-    lightGreen: '#8AB402',
-    cream: '#FFF3EC',
-    textGrey: 'hsl(0, 0%, 50%)',
-    shadowColor: 'hsl(0, 0%, 0%)',
-    ratingYellow: 'hsl(48, 91%, 61%)' // Black shadow for light theme
+    background: '#F5F9FF',       // Updated to lightGrey background
+    backgroundSecondary: '#E8EFF9', // Slightly darker version of lightGrey
+    primary: '#f78620',          // Keeping original primary color
+    primaryGrey: '#646898',      // Updated to darkGrey
+    secondaryDark: '#2C2E49',    // Updated to black
+    borderGrey: '#86A0CA',       // Updated to grey
+    textBlack: '#2C2E49',        // Updated to black
+    secondary: '#296AEB',        // Updated to blue
+    red: '#F84848',              // Updated red
+    lightRed: '#FCF0F0',         // Added lightRed
+    green: '#18F27A',            // Updated green
+    lightGreen: '#72FFB1',       // Updated lightGreen
+    cream: '#FFF3EC',            // Keeping original cream color
+    textGrey: '#646898',         // Updated to darkGrey for text
+    shadowColor: 'rgba(44, 46, 73, 0.1)', // Using black with transparency
+    darkGrey: '#646898',         // Added darkGrey
+    grey: '#86A0CA',             // Added grey
+    lightGrey: '#F5F9FF',        // Added lightGrey
+    black: '#2C2E49',            // Added black
+    blue: '#296AEB',             // Added blue
+    lightBlue: '#C3DBFF00',      // Added lightBlue
+    ratingYellow: '#FFBF1A',     // Keeping original rating yellow
 };
 
 export const DEFAULT_LIGHT_THEME_ID = 'default-light';
 
+/**
+ * Complete light theme configuration including spacing, border radius, and font sizes
+ */
 export const DEFAULT_LIGHT_THEME: Theme = {
     id: DEFAULT_LIGHT_THEME_ID,
     color: DEFAULT_LIGHT_COLOR_THEME,
-
+    
     // Spacing scale (in pixels)
     spacing: {
         xs: 4,
@@ -33,7 +47,7 @@ export const DEFAULT_LIGHT_THEME: Theme = {
         xl: 32,
         xxl: 48
     },
-
+    
     // Border Radius scale
     borderRadius: {
         sm: 4,
@@ -42,7 +56,7 @@ export const DEFAULT_LIGHT_THEME: Theme = {
         xl: 16,
         full: 9999
     },
-
+    
     // Font Sizes scale
     fontSize: {
         xs: 10,
