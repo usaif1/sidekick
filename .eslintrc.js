83a291d4f620c4148a4f5e7df6582ea53dbcbcca
usaif1 <saif.ullah@fuelbuddy.in>
2025-03-09 04:09:41 +0530

    modified eslint rules to allow inline styles and show warning on any

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    // Disable inline styles warning
+    'react-native/no-inline-styles': 'off',
+
+    // Show warnings for TypeScript 'any' type
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
 };
