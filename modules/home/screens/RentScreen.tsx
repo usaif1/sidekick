7cb5da33f297680e02c6ef3cc6acf30f01145289
usaif1 <saif.ullah@fuelbuddy.in>
2025-03-10 03:38:27 +0530

    fix map on android

--- modules/home/screens/RentScreen.tsx
+++ modules/home/screens/RentScreen.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
-import { View, StyleSheet, PermissionsAndroid, Platform } from 'react-native';
-import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import React, {useEffect} from 'react';
+import {View, StyleSheet, PermissionsAndroid, Platform} from 'react-native';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 import Geolocation from 'react-native-geolocation-service';
 import useLocationStore from '../store/locationStore';
 
@@ -9,12 +9,12 @@ import UserPointerIcon from '../assets/userPointer.svg';
 import ButtonWithIcon from '@/components/ButtonWithIcon';
 import ScanIcon from '../assets/scanIcon.svg';
 
-import { scooterHubs } from '../data/scooterHubs';
+import {scooterHubs} from '../data/scooterHubs';
 import UserLocationMarker from '../components/UserLocationMarker';
 import NearestHubMarker from '../components/NearestHubMarker';
 
 const RentScreen: React.FC = () => {
-  const { latitude, longitude, setLocation } = useLocationStore();
+  const {latitude, longitude, setLocation} = useLocationStore();
 
   useEffect(() => {
     const requestLocationPermission = async () => {
@@ -35,26 +35,27 @@ const RentScreen: React.FC = () => {
         position => {
           const lat = position.coords.latitude;
           const lng = position.coords.longitude;
-          console.log('Current location:', { latitude: lat, longitude: lng });
+          console.log('Current location:', {latitude: lat, longitude: lng});
           setLocation(lat, lng);
         },
         error => console.log('Error getting current location:', error),
-        { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 },
+        {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
       );
 
       Geolocation.watchPosition(
         position => {
           const lat = position.coords.latitude;
           const lng = position.coords.longitude;
-          console.log('Updated location:', { latitude: lat, longitude: lng });
+          console.log('Updated location:', {latitude: lat, longitude: lng});
           setLocation(lat, lng);
         },
         error => console.log('Error watching location:', error),
-        { enableHighAccuracy: true, distanceFilter: 1, interval: 1000 },
+        {enableHighAccuracy: true, distanceFilter: 1, interval: 1000},
       );
     };
 
     requestLocationPermission();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
@@ -63,27 +64,30 @@ const RentScreen: React.FC = () => {
         style={styles.map}
         provider={PROVIDER_GOOGLE}
         followsUserLocation={true}
+        key={'AIzaSyA4_-URnAPZCngJLIbQ9mhMuy-Lq1-iz-Y'}
         region={{
           latitude,
           longitude,
           latitudeDelta: 0.02,
           longitudeDelta: 0.02,
-        }}
-      >
+        }}>
         <UserLocationMarker latitude={latitude} longitude={longitude} />
 
         {scooterHubs.map(hub => (
-          <NearestHubMarker key={hub.id} latitude={hub.latitude} longitude={hub.longitude} name={hub.name} />
+          <NearestHubMarker
+            key={hub.id}
+            latitude={hub.latitude}
+            longitude={hub.longitude}
+            name={hub.name}
+          />
         ))}
-
       </MapView>
 
       <View style={styles.bottomContainer}>
         <ButtonWithIcon
           variant="primary"
           onPress={() => console.log('Unlock Pressed')}
-          IconComponent={ScanIcon}
-        >
+          IconComponent={ScanIcon}>
           Unlock
         </ButtonWithIcon>
       </View>
